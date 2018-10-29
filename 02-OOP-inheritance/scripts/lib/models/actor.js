8701cdb05e9db08a17c6a673725cccf39cfe501b
lazcanogaston <lazcanogaston95@gmail.com>
2018-10-29 14:06:23 -0300

    exercise 5

---
+++ 02-OOP-inheritance/scripts/lib/models/actor.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+class Actor {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+}
+
+exports.default = Actor;
