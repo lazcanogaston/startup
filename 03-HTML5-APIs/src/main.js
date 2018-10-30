0051f20ccd6b1f1f699d6f7ff3cbd25e61cae5ea
lazcanogaston <lazcanogaston95@gmail.com>
2018-10-30 15:26:29 -0300

    exercise 2

--- 03-HTML5-APIs/src/main.js
+++ 03-HTML5-APIs/src/main.js
@@ -94,3 +94,25 @@ function deleteDb(){
 
 
 //EXERCISE 2
+
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var files = evt.dataTransfer.files; 
+    var reader = new FileReader();  
+    reader.onload = function(event) {            
+         document.getElementById('text').value = event.target.result;
+    }        
+    reader.readAsText(files[0],"UTF-8");
+  }
+
+  function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; 
+  }
+
+  var dropZone = document.getElementById('text');
+  dropZone.addEventListener('dragover', handleDragOver, false);
+  dropZone.addEventListener('drop', handleFileSelect, false);
