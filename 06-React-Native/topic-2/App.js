8b8726571f4c6169d1984ac29a634307334e3309
lazcanogaston <lazcanogaston95@gmail.com>
2018-11-10 17:04:49 -0300

    exercise 7 topic-2

--- 06-React-Native/topic-2/App.js
+++ 06-React-Native/topic-2/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import styles from './styles/styles.js';
 
 
 export default class App extends React.Component {
+  
   render() {
+    var pic = require ('./media/facebook.png');
     return (
       <View style={styles.container}>
-        <View style={[styles.Box_1, styles.bRadius]}/>
-        <View style={[styles.Box_2, styles.bRadius]}/>
-        <View style={[styles.Box_3, styles.bRadius]}/>  
+      
+        <Text style={styles.title}>TITLE</Text>
+        <View style={styles.articleWrapper}>   
+          <Text style={styles.article}> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet, quasi veniam. Placeat quasi facilis provident rerum, beatae deserunt. Molestias ex molestiae cumque hic! Suscipit deserunt odit nulla, tenetur cupiditate reiciendis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, ad eum! Animi nobis consequatur nesciunt inventore porro suscipit sint totam eaque aliquid facere rerum, iste optio odit, ratione, nostrum autem?</Text>
+          <Image source= {require('./media/landscape4.jpg')} style={styles.articlePhoto}/>
+        </View>
+        
+         <View style={styles.imgSection}>
+          <Image source={pic} style={styles.img}/>  
+          <Image source={pic} style={styles.img}/>
+          <Image source={pic} style={styles.img}/>
+          <Image source={pic} style={styles.img}/> 
+        </View> 
       </View>
     );
   }
--- 06-React-Native/topic-2/styles/styles.js
+++ 06-React-Native/topic-2/styles/styles.js
@@ -9,13 +9,13 @@ const styles = StyleSheet.create({
                 backgroundColor: 'grey'
             },
             android: {
-                backgroundColor: 'green'
+                backgroundColor: 'white'
             }
         }),
         
         flexDirection:'column', //change to 'column' to see how the blocks changes their positions and complete all the device height 
         alignItems: 'flex-start',
-        justifyContent: 'space-around',
+        justifyContent: 'flex-start',
         
       },    
     Box_1:{
@@ -42,6 +42,43 @@ const styles = StyleSheet.create({
     },
     bRadius:{
         borderRadius: 50
+    },
+
+    title:{
+        fontFamily: 'serif', 
+        fontSize: 25 ,
+        fontWeight: 'bold', 
+        color: 'black',
+        alignSelf: 'center',
+        top: 7
+    },
+    articleWrapper:{
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: '5%',
+        width: '100%'     
+    },
+    article:{
+        width: '60%',
+        fontFamily: 'serif', 
+        fontSize: 12 ,
+        fontWeight: 'bold', 
+        color: 'black',
+        marginLeft: '3%'
+    },
+    imgSection:{
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+
+    },
+    img:{
+        height: 100,
+        width: 100
+    },
+    articlePhoto:{
+        width:'30%',
+         height: '30%'
     }
 
 });
